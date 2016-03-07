de9fcba9655ec3f0d379bebd10d0f4c24f928c61
emarmol <erika.marmol@tufts.edu>
2016-03-07 16:53:12 -0500

    fixed typos, add condition for request statuses

--- messages/lab.js
+++ messages/lab.js
@@ -1,8 +1,6 @@
 // Your JavaScript goes here...
 
 var myRequest = new XMLHttpRequest();
-var result = "";
-var raw;
 
 function parse() {
 
@@ -12,18 +10,35 @@ myRequest.open("GET", "data.json", true);
 myRequest.onreadystatechange = function () {
 
     // Step 3: Data is ready --there is a response!
-    if (myRequest.readyState == 4) {
+    /*    if (myRequest.readyState == 4) {
 	result = "";
 	raw = myRequest.responseText;
-	theScheduleData = JSON.parse(raw);
-	elem = document.getElementById("messages");
+	myData = JSON.parse(raw);
+	msg = document.getElementById("messages");
 
-	result = theScheduleData["content"][0] + "&nbsp" + theScheduleData["username"][0];
-	/*for (i = 0; i < theScheduleData["1"]["Trips"].length; i++) {
-	    result += "<p>Next Red Line train to " + theScheduleData["TripList"]["Trips"][i]["Predictions"][0]["Stop"] + ", " + theScheduleData["TripList"]["Trips"][i]["Destination"] + " bound, will come in " + theScheduleData["TripList"]["Trips"][i]["Predictions"][0]["Seconds"] + " seconds</p>";
-	    }*/
+	result = myData["content"][0] + "&nbsp" + myData["username"][0];
 	elem.innerHTML = result;
+	}*/
+
+
+    if (myRequest.readyState == 4 && myRequest.status == 200) {
+	console.log("Got the data back!");
+	data = myRequest.responseText;
+	console.log(data);
+	raw = JSON.parse(data);
+	elem = document.getElementById("message");
+	elem.innerHTML = "<p>Carmen Sandiego was last seen at " + loc["description"] + "</p>";
     }
+    else if (myRequest.readyState == 4 && myRequest.status != 200) {
+	// think 404 or 500
+	document.getElementById("message").innerHTML = "<p>Whoops, something went terribly wrongo</p>";
+    }
+    else {
+	console.log("In progress...");
+    }
+
+
+
 };
 
 // Step 3: Fire off the request
