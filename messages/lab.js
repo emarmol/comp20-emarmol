867d6eb1f694cd3a99f07b35182b8171e75f892f
emarmol <erika.marmol@tufts.edu>
2016-03-09 02:21:40 -0500

    took out console.log statements

--- messages/lab.js
+++ messages/lab.js
@@ -6,31 +6,17 @@ function parse() {
 
 myRequest.open("GET", "data.json", true);
 
-
 myRequest.onreadystatechange = function () {
-
     // Step 3: Data is ready --there is a response
     if (myRequest.readyState == 4 && myRequest.status == 200) {
-	//console.log("Got the data back!");
 	data = myRequest.responseText;
-	//console.log(data);
 	msg = JSON.parse(data);
 	elem = document.getElementById("messages");
 	elem.innerHTML = "<p id='first'>" + msg[0]["content"] + "&nbsp<span class='user'>" + msg[0]["username"] + "</span></p>" + "<br/>" + "<p id='second'>" + msg[1]["content"] + "&nbsp<span class='user'>" + msg[1]["username"] + "</span></p>";
     }
     else if (myRequest.readyState == 4 && myRequest.status != 200) {
-	// think 404 or 500
-	document.getElementById("message").innerHTML = "<p>Whoops, something went terribly wrongo</p>";
+	document.getElementById("message").innerHTML = "<p>Something went wrong</p>";
     }
-    else {
-	//console.log("In progress...");
-    }
-
-
-
 };
-
-// Step 3: Fire off the request
-myRequest.send(null); // null means no data nec to send
-
+ myRequest.send(null); // null means no data nec to send
 }
